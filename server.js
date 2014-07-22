c32be3d2f810af58139e6aa57158bf12a79a3a2c
Robert Guo <robert.guo@10gen.com>
2014-07-22 16:38:41 -0400

    use server

--- server.js
+++ server.js
@@ -1,6 +1,36 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:8000/');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var app = express();
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(session({ secret:'stocktest', cookie: { maxAge: 60000 }}));
+app.use(function(req, res, next) {
+  var sess = req.session;
+  if (sess.views) {
+    sess.views++;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>views: ' + sess.views + '</p>');
+    res.write('<p>expires in: ' + (sess.cookie.maxAge / 1000) + 's</p>');
+    next();
+  } else {
+    sess.views = 1
+    res.send('welcome to the session demo. refresh!');
+    next();
+  }
+});
+
+app.get('/', function(req, res) {
+    /*console.log(req.param('user'));
+    console.log(req.param('pass'));
+    console.log("happy");*/
+    res.send('welcome get');
+});
+
+app.post('/login', function(req, res) {
+    var post_body = req.body;
+});
+
+app.listen(8000);
+console.log('server started on port' + 8000);
