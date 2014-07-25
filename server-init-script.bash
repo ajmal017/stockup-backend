505b40e8b08b558a4f6ed1660aa6c0e4dd28eee9
Robert Guo <robert.guo@10gen.com>
2014-07-25 18:11:33 -0400

    more init script modification

--- server-init-script.bash
+++ server-init-script.bash
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 #generate ssh key
 cd ~/.ssh
 ssh-keygen -t rsa -C "robert.guo@10gen.com" -f id_rsa -N ''
 echo "please add this key to github"
+read placeholder
 echo id_rsa
 
 sudo apt-get update
@@ -10,8 +13,7 @@ echo "Y" | sudo apt-get upgrade
 echo "Y" | sudo apt-get install apache2
 echo "Y" | sudo apt-get install php5
 echo "Y" | sudo apt-get install nodejs
-echo "Y" | sudo apt-get install emacs
-
+echo "Y" | sudo apt-get install mongodb
 echo "Y" | sudo apt-get install git
 
 echo "alias gclone='git clone git@github.com:guoyr/stockup-backend.git'" >> ~/.bashrc
@@ -28,4 +30,29 @@ sudo cp ~/.ssh/id_rsa /var/www/.ssh/
 sudo chown -R www-data:www-data /var/www/
 
 sudo -Hu www-data git clone git@github.com:guoyr/stockup-backend.git /var/www/stockup-backend
+sudo -Hu www-data ln -s /var/www/stockup-backend/deploy.php /var/www/html/deploy.php
+
+echo "Y" | sudo apt-get install emacs
+
+sudo fdisk -l
+echo -n "What is the name of the disk? "
+read diskName
+echo "n                                                                                                            
+p                                                                                                                  
+1
+
+
+w                                                                                                                  
+" | sudo fdisk /dev/$diskName
+
+sudo mkfs -t ext4 /dev/"$diskName"1
+sudo mkdir /data-drive
+sudo mount /dev/"$diskName"1 /data-drive
+
+line=$(sudo -i blkid|tail -1)
+words=($line)
+UUID=${line[1]}
+# TODO: add to /etc/fstab
+
+
 
