c90ba71055cdac68f7a8590bb84635a5623a1bb5
Robert Guo <robert.guo@10gen.com>
2014-09-10 20:27:42 -0400

    add apns certs and basic server

--- servers/apns_server.py
+++ servers/apns_server.py
@@ -1,3 +1,35 @@
-from lib.apns import APNs
+import time
+from lib.apns import APNs, Payload
+
+import config
 
 apns = APNs(use_sandbox=True)
+
+from tornado import ioloop
+
+apns = APNs(use_sandbox=True, cert_file='certs/stockup_dev_cert.pem', key_file='certs/stockup_key_nopass.pem')
+
+def success():
+    print("Sent push message to APNS gateway.")
+    ioloop.IOLoop.instance().stop()
+
+def send():
+    identifier = 1
+    expiry = time.time()+3600
+    token_hex = config.TEST_IPAD_TOKEN
+    payload = Payload(alert="Hello World!", sound="default", badge=1)
+    apns.gateway_server.send_notification(identifier, expiry, token_hex, payload, success)
+
+def on_response(status, seq):
+    print "sent push message to APNS gateway error status %s seq %s" % (status, seq)
+
+def on_connected():
+    apns.gateway_server.receive_response(on_response)
+
+# Connect the apns
+apns.gateway_server.connect(on_connected)
+
+# Wait for the connection and send a notification
+ioloop.IOLoop.instance().add_timeout(time.time()+5, send)
+
+ioloop.IOLoop.instance().start()
--- servers/config.py
+++ servers/config.py
@@ -3,3 +3,5 @@ import socket
 DEBUG = True
 if 'stockup' in socket.gethostname(): 
 	DEBUG = False
+
+TEST_IPAD_TOKEN = 'bead382dc014c9f73246f9f5b6d606ddf87b1237701eca8e743cf3641089c79d'
