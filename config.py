42100f73fdeb7bc2ae1d97e0da41e9c26d814b94
Robert Guo <robert.guo@10gen.com>
2015-02-28 23:32:39 -0500

    cleanup config

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-import socket
-
 TEST_IPAD_TOKEN = 'bead382dc014c9f73246f9f5b6d606ddf87b1237701eca8e743cf3641089c79d'
 
 COOKIE_KEY = "2BGQuaOqQwOOjPRhuYwgk95iG767I0xPmstRiVNMzDg="
 
 DEBUG = True
+
 
 def debug_log(logger, msg):
     if DEBUG:
