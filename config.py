70640695525c00b0177e942930f727d61639297c
root <codepython>
2015-03-30 21:54:34 +0800

    default db host

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ COOKIE_KEY = "2BGQuaOqQwOOjPRhuYwgk95iG767I0xPmstRiVNMzDg="
 
 DEBUG = True
 
-define("port", default=99890, help="run on the given port", type=int)
+define("port", default=9990, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode", type=bool)
 define("run_crawler", default=True, help="run the crawler", type=bool)
 define("run_server", default=True, help="run the server", type=bool)
 define("cookie_secret", default=COOKIE_KEY,
        help="the key to generate secure cookies", type=str)
-define("dbhost", default="119.29.16.193", help="mongodb host ip address ", type=str)
+define("dbhost", default="127.0.0.1", help="mongodb host ip address ", type=str)
 
 def debug_log(logger, msg):
     if DEBUG:
